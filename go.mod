2f74424bb1aa225e354662397e8d6cfb994bd058
AA AA <armalyalbert@gmail.com>
2021-03-10 20:13:17 -0500

    fmt

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Armaly/armplay-discord
 
 go 1.16
 
-require github.com/bwmarrin/discordgo v0.23.2
+require (
+	github.com/bwmarrin/discordgo v0.23.2
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,22 +2,34 @@ package main
 
 import (
 	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 	"log"
-
-	"github.com/bwmarrin/discordgo")
+	"os"
+)
 
 func init() {
+
 	fmt.Println("Creating Armplay!")
-	discord, err := discordgo.New("Bot " + "authentication token")
+
+	//Load .env File
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Failure loading .env: %v", err)
+	}
+
+	botToken := os.Getenv("TOKEN")
+
+	discord, err := discordgo.New("Bot " + botToken)
 	if err != nil {
 		log.Fatal("Incorrect Credentials: %v", err)
 	}
 
 }
+
+var 
 func main() {
 
 	fmt.Println("Hello World!")
-
-	
 
 }
