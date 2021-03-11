a926cf8286cc85519b30ff67bfc78de735cf7e7e
AA AA <armalyalbert@gmail.com>
2021-03-10 19:31:31 -0500

    Setting mod, sum, transitioning to functions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Armaly/armplay-discord
 
 go 1.16
+
+require github.com/bwmarrin/discordgo v0.23.2
--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
+	"github.com/bwmarrin/discordgo")
+
+func init() {
+	fmt.Println("Creating Armplay!")
+	discord, err := discordgo.New("Bot " + "authentication token")
+	if err != nil {
+		log.Fatal("Incorrect Credentials: %v", err)
+	}
+
+}
 func main() {
 
 	fmt.Println("Hello World!")
+
+	
 
 }
